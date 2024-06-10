f0d1975c979f7a5ab382545904276d6d4d5bc99d
1katuramu <108106295+1katuramu@users.noreply.github.com>
2024-06-10 11:22:57 +0300

    Create pipcw.py

---
+++ Afternoon Session/__pycache__/pipcw.py
@@ -0,0 +1,25 @@
+import camelcase
+# Python PIP
+# PIP is a package manager for Python packages
+# What is a Package?
+# A package contains all the files you need for a module.
+# Modules are Python code libraries you can include in your project.
+# Check if PIP is Installed
+# pip --version
+# Download a Package
+# pip install camelcase
+# Using a Package
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
+
+# Remove a Package
+# pip uninstall camelcase
+
+# List Packages
+# pip list
+
+
